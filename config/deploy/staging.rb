49dacc5fb94567fa221ca31d5eb2127db5f72661
Chet Swalina and Luke Niedermyer <developers+chet+luke@westarete.com>
2014-05-16 16:43:25 -0400

    Update hosts for deployment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,11 +1,11 @@
 # The directory that we're deploying to on the remote host.
-set :deploy_to, "/var/www/sites/staging.etdflow.com"
+set :deploy_to, "/var/www/sites/staging.etdflow.westarete.com"
 
 # Tell capistrano to use the staging environment. This is key for running 
 # the database migrations via "cap staging deploy:migrations".
 set :rails_env, "staging"
 
 # The hosts that we're deploying to.
-role :app, "etdflow.westarete.com"
-role :web, "etdflow.westarete.com"
-role :db,  "etdflow.westarete.com", :primary => true
+role :app, "staging.etdflow.westarete.com"
+role :web, "staging.etdflow.westarete.com"
+role :db,  "staging.etdflow.westarete.com", :primary => true
