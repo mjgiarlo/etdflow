75016d467ea7b8ced1791802a4d9ad3cd0d4a6f4
Chet Swalina and Luke Niedermyer <developers+chet+luke@westarete.com>
2014-05-22 14:32:31 -0400

    Root to the home page
    This way, our initial deploy to staging won't blow up
    on the home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  root :to => "catalog#index"
+  get '/catalog' => "catalog#index"
   blacklight_for :catalog
   devise_for :users
 
-  get '/home' => 'static#home', as: :home
+  root :to => 'static#home'
 
 end
