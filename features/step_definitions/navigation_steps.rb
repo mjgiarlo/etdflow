6efd32035c21be795d2bde71bf49308e212172a4
Chet Swalina <cswalina@gmail.com>
2014-06-11 11:30:09 -0400

    Group generalized step definitions by function
    Trying to create an organized library of common cucumber
    step definitions so that future tests are easier to write
    and maintain.

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -1,3 +1,5 @@
+# Generalized step definitions for navigation
+
 Given /^I am on (.*)$/ do |page_name|
   visit path_to(page_name)
 end
@@ -6,33 +8,15 @@ When /^I go to (.*)$/ do |page_name|
   visit path_to(page_name)
 end
 
+When(/^I click the "(.*?)" link$/) do |link|
+  click_link link
+end
+
 Then /^I should be on (.*)$/ do |page_name|
   current_path = URI.parse(current_url).path
   if current_path.respond_to? :expect
     expect(current_path).to eq( path_to(page_name) )
   else
     assert_equal path_to(page_name), current_path
-  end
-end
-
-When(/^I click the "(.*?)" link$/) do |link|
-  click_link link
-end
-
-When /^(?:|I )fill in "([^"]*)" with "([^"]*)"(?: within "([^"]*)")?$/ do |field, value, selector|
-  with_scope(selector) do
-    fill_in(field, :with => value)
-  end
-end
-
-When /^(?:|I )check "([^"]*)"(?: within "([^"]*)")?$/ do |field, selector|
-  with_scope(selector) do
-    check(field)
-  end
-end
-
-When /^(?:|I )press "([^"]*)"(?: within "([^"]*)")?$/ do |button, selector|
-  with_scope(selector) do
-    click_button(button)
   end
 end
