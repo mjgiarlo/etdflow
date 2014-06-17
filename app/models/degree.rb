7b4fed1e18412f2212b91a41296692fdef74683f
Chet Swalina and Luke Niedermyer <developers+chet+luke@westarete.com>
2014-06-17 16:32:04 -0400

    Allow for configuration of degree types
    We're using the Graduate School defaults:
    "Master Thesis" and "Dissertation".

--- app/models/degree.rb
+++ app/models/degree.rb
@@ -1,16 +1,10 @@
 class Degree <  ActiveRecord::Base
 
-  DEGREE_TYPES = [
-                  "Dissertation",
-                  "Thesis",
-                  "Master"
-                 ].freeze
-
   validates_presence_of :name
   validates_presence_of :description
   validates_presence_of :degree_type
 
-  validates_inclusion_of :degree_type,  in: DEGREE_TYPES
+  validates_inclusion_of :degree_type,  in: Etdflow::Application.config.degree_types
 
   validates_uniqueness_of :name
 
