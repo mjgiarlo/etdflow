e555a95513bba344e849e3e6768c3de8a49c7f7e
Chet Swalina <cswalina@gmail.com>
2014-07-25 10:28:00 -0400

    FormatReview instances will need their submission
    The submission should provide the metadata needed in the
    ActiveFedora FormatReviewPaper (to be implemented).

--- app/models/mock_format_review.rb
+++ app/models/mock_format_review.rb
@@ -1,10 +1,16 @@
 class MockFormatReview
   include ActiveModel::Model
 
+  attr_reader :submission
+
   @@saved_files = []
 
   def self.saved_files
     @@saved_files
+  end
+
+  def initialize(submission=nil)
+    @submission = submission
   end
 
   def save(files)
