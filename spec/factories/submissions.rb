958176e3d165363c96a7c25e9e68fe3e2f5f1c26
Chet Swalina and Luke Niedermyer <developers+chet+luke@westarete.com>
2014-07-31 13:10:45 -0400

    Refactor degree type traits into submission factory

--- spec/factories/submissions.rb
+++ spec/factories/submissions.rb
@@ -8,4 +8,11 @@ FactoryGirl.define do
     year Date.today.year
   end
 
+  trait :dissertation do
+    association :degree, factory: :degree, degree_type: "Dissertation"
+  end
+
+  trait :master_thesis do
+    association :degree, factory: :degree, degree_type: "Master Thesis"
+  end
 end
