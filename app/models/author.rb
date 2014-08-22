a9c9fcaaa49c65ff9ccca03fb1d95c0cb8a81a75
Chet Swalina and Luke Niedermyer <developers+chet+luke@westarete.com>
2014-08-22 16:40:26 -0400

    Don't validate for presence of author middle name

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,7 +5,6 @@ class Author <  ActiveRecord::Base
   validates_presence_of :access_id,
                         :first_name,
                         :last_name,
-                        :middle_name,
                         :alternate_email_address,
                         :psu_email_address,
                         :phone_number,
