a3a69a8e1fba096f4f986224652bdfbd1a3ebb87
Luke Niedermyer <luke@westarete.com>
2014-09-10 22:30:12 -0400

    Add a submitted format review filter
    This also improves the filters and adds a non-linking
    placeholder for filters when there are no submissions
    at the appropriate status

--- app/presenters/admin/submissions_dashboard_view.rb
+++ app/presenters/admin/submissions_dashboard_view.rb
@@ -10,7 +10,8 @@ class Admin::SubmissionsDashboardView
 
   def filters
     [
-      format_review_is_incomplete_filter
+      format_review_is_incomplete_filter,
+      format_review_is_submitted_filter
     ]
   end
 
@@ -18,7 +19,20 @@ class Admin::SubmissionsDashboardView
 
   def format_review_is_incomplete_filter
     submissions = Submission.send(@degree_type).format_review_is_incomplete
-    ("<a href='" + "/admin/#{@degree_type}/format_review_incomplete" + "'> Format Review is Incomplete <span>" + submissions.count.to_s + "</span></a>").html_safe unless submissions.empty?
+    if submissions.empty?
+     ("<div class='panel panel-default no-submissions'><div class='panel-heading'><h3 class='panel-title'>Format Review is Incomplete <span class='badge pull-right'>0</span></h3></div><div class='panel-body'>Submissions whose format review has not yet been submitted or are currently rejected.</div></a>").html_safe
+    else
+      ("<a href='" + "/admin/#{@degree_type}/format_review_incomplete" + "' class='panel panel-default filter'><div class='panel-heading'><h3 class='panel-title'>Format Review is Incomplete <span class='badge pull-right'>" + submissions.count.to_s + "</span></h3></div><div class='panel-body'>Submissions whose format review has not yet been submitted or are currently rejected.</div></a>").html_safe
+    end
+  end
+
+  def format_review_is_submitted_filter
+    submissions = Submission.send(@degree_type).format_review_is_submitted
+    if submissions.empty?
+      ("<div class='panel panel-default no-submissions'><div class='panel-heading'><h3 class='panel-title'>Format Review is Submitted <span class='badge pull-right'>0</span></h3></div><div class='panel-body'>Submissions whose format review is currently waiting to be approved or rejected.</div></a>").html_safe
+    else
+      ("<a href='" + "/admin/#{@degree_type}/format_review_submitted" + "' class='panel panel-default filter'><div class='panel-heading'><h3 class='panel-title'>Format Review is Submitted <span class='badge pull-right'>" + submissions.count.to_s + "</span></h3></div><div class='panel-body'>Submissions whose format review is currently waiting to be approved or rejected.</div></a>").html_safe
+    end
   end
 
 end
