f779c29015076cd33c252fd61c4854ab588e326a
Chet Swalina and Luke Niedermyer <developers+chet+luke@westarete.com>
2014-09-24 10:33:31 -0400

    Remove mock depositor
    Not clear whether we need this.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,3 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
-
-# Stub out calls to Depositor
-Depositor = MockDepositor
