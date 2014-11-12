5b50b8105e210f48cc48105d4ac97420933f6081
Scott Woods <scott@westarete.com>
2014-11-12 07:12:27 -0500

    Force IPv4 for deploying to QA
    We can revert this commit when IPv6 is allowed from the CI build
    machine.

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,4 +1,6 @@
-role :web,  "etda1qa.dlt.psu.edu"
-role :app,  "etda1qa.dlt.psu.edu"
-role :solr, "etda1qa.dlt.psu.edu" # This is where resolrize will run
-role :db,   "etda1qa.dlt.psu.edu", primary: true # This is where Rails migrations will run
+# We use the ipv4 address for etda1qa.dlt.psu.edu to avoid an ipv6 connection,
+# which is currently getting blocked due to firewall rules.
+role :web,  "128.118.88.145"
+role :app,  "128.118.88.145"
+role :solr, "128.118.88.145" # This is where resolrize will run
+role :db,   "128.118.88.145", primary: true # This is where Rails migrations will run
